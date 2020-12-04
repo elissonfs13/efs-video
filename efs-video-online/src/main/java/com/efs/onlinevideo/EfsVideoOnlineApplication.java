cabac6c15244ad196886650bc05abca2a8c95846
Elisson Silva <elissonfs@gmail.com>
2020-12-04 14:48:49 -0300

    Project efs-video-online

---
+++ efs-video-online/src/main/java/com/efs/onlinevideo/EfsVideoOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.efs.onlinevideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfsVideoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfsVideoOnlineApplication.class, args);
+	}
+
+}
---
+++ efs-video-online/src/test/java/com/efs/onlinevideo/EfsVideoOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efs.onlinevideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfsVideoOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
